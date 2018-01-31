82117ccbe44396dec25caf938b47ddd133b72826
daoyuan <1833063210@qq.com>
2018-01-31 18:20:38 +0800

    优化注解

--- boot-web/src/main/java/cn/amos/web/BootWebApplication.java
+++ boot-web/src/main/java/cn/amos/web/BootWebApplication.java
@@ -3,7 +3,6 @@ package cn.amos.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("cn.amos")
 @MapperScan("cn.amos.dao.mappers")
-@SpringBootApplication
+@EnableAutoConfiguration
 public class BootWebApplication {
 
     public static void main(String[] args) {
