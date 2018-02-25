8214ea2970cfd683f5a75a6ae7d5fd7254f77c52
DaoyuanWang <1833063210@qq.com>
2018-02-25 19:31:19 +0800

    master保持最新、最完善代码

--- boot.sql
+++ boot.sql
@@ -15,25 +15,6 @@ USE boot;
 -------------***  分支 MASTER SQL   ***-----------------
 -------------*** 功能：基础boot功能 ***-----------------
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增id',
-  `NAME` varchar(128) DEFAULT NULL COMMENT '用户名',
-  `AGE` tinyint(4) DEFAULT NULL COMMENT '年龄',
-  `PHONE_NO` varchar(11) DEFAULT NULL COMMENT '手机号',
-  `ADDRESS` varchar(255) DEFAULT NULL COMMENT '地址',
-  `IDENTITY_NO` varchar(18) DEFAULT NULL COMMENT '身份证号',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
-
-INSERT INTO `user` VALUES ('1', '啦啦啦', '18', '18937128888', '上海市黄浦区西藏南路', '411388199909299999');
-
--- ********************************************************************************************************************************************
--- --------------------------------------------------------------------------------------------------------------------------------------------
--- ********************************************************************************************************************************************
--------------***   分支 DEVELOP-PAGE SQL    ***-----------------
--------------***   功能：增加分页实现       ***-----------------
-
 DROP TABLE IF EXISTS `apply`;
 CREATE TABLE `apply` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增id',
